e27bed1013af853cb15e51720a65db77fcebbea3
MartinDanielCP <MartinCalderon92@outlook.com>
2021-04-22 22:18:10 -0300

    MyAccount repair.

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -2,11 +2,17 @@ import React from 'react';
 import UnderConstruction from '../images/underConstruction.png';
 
 export default ()=>{
+    let imgStyle={
+        width: '50 vh'
+    }
+
     return(
         <>
             <img
+                className='center'
                 src={UnderConstruction}
                 alt='Website Under Construction'
+                style={imgStyle}
             />
         </>
     )
