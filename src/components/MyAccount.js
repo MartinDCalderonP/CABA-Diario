282d72033daac44cb53057ce4819b0799ff8af42
MartinDanielCP <MartinCalderon92@outlook.com>
2021-04-22 22:39:03 -0300

    MyAccount under construction.

--- src/components/MyAccount.js
+++ src/components/MyAccount.js
@@ -3,17 +3,18 @@ import UnderConstruction from '../images/underConstruction.png';
 
 export default ()=>{
     let imgStyle={
-        width: '50 vh'
+        width: '50vh'
     }
 
     return(
-        <>
+        <div
+            className= 'center'
+        >
             <img
-                className='center'
                 src={UnderConstruction}
                 alt='Website Under Construction'
                 style={imgStyle}
             />
-        </>
+        </div>
     )
 }
